6c8ed88add5176c0d97b9042a6d15990d7e4127a
txiong000 <tkx422@gmail.com>
2018-11-09 11:28:27 -0600

    mvp complete

--- server.js
+++ server.js
@@ -85,6 +85,72 @@ server.get('/api/actions', (req, res) => {
 
 
 
+server.get('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    actions.get(id)
+            .then(action => {
+                res.status(200).json(action)
+            })
+            .catch(err => {
+                res.status(500).json({ message: "error no id by that number"})
+            })
+})
+
+server.post('/api/actions', (req, res) => {
+    const {project_id, description, notes} = req.body;
+    actions.insert({project_id, description, notes})
+            .then(action => {
+                res.status(200).json(action)
+            })
+            .catch(err => {
+                res.status(500).json({ message: "Can not add new action"})
+            })
+})
+
+
+
+server.put('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    actions.update(id, changes)
+            .then(action => {
+                if(action) {
+                    res.status(200).json(action);
+                } else {
+                    res.status(404).json({ message: 'project not found'})
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ message: 'failed to update project'})
+            })
+})
+
+
+server.delete('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    actions.remove(id)
+            .then(action => {
+               if(action) {
+                res.status(200).json(action)
+               } else {
+                 res.status(400).json({ message: "no matching id"})
+               }
+            })
+            .catch(err => {
+                res.status(500).json({ message: "error no id match to delete"})
+            })
+})
+
+server.get('/api/projects/action/:id', (req, res) => {
+    const { id } = req.params;
+    projects.getProjectActions(id)
+            .then(project => {
+                res.status(200).json(project)
+            })
+            .catch(err => {
+                res.status(500).json({ message: "unable to retrieve project actions"})
+            })
+})
 
 
 
